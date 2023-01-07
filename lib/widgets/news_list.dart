d259dc165518267f98ffa7fc9e4b7aa41c02ff5b
Yuler Figueroa <yuleralex1@gmail.com>
2023-01-06 21:24:01 -0500

    view news with images, body and footer

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -30,7 +30,13 @@ class _NewsItem extends StatelessWidget {
       children: [
         _TopBarCard(article: article, index: index),
         _CardTitle(article: article),
-        _CardImage(article: article)
+        _CardImage(article: article),
+        _CardBody(article: article),
+        SizedBox(
+          height: 10,
+        ),
+        Divider(),
+        _CardFooter(),
       ],
     );
   }
@@ -88,7 +94,64 @@ class _CardImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('article image here'),
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        child: Container(
+          child: (article.urlToImage != null)
+              ? FadeInImage(
+                  placeholder: AssetImage('assets/img/giphy.gif'),
+                  image: NetworkImage(article.urlToImage!),
+                )
+              : Image(image: AssetImage('assets/img/no-image.png')),
+        ),
+      ),
+    );
+  }
+}
+
+class _CardBody extends StatelessWidget {
+  final Article article;
+
+  const _CardBody({super.key, required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Text(article.description != null ? article.description! : ''),
+    );
+  }
+}
+
+class _CardFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RawMaterialButton(
+            fillColor: myTheme.colorScheme.secondary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Icon(Icons.star_border),
+            onPressed: () {},
+          ),
+          RawMaterialButton(
+            fillColor: myTheme.colorScheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Icon(Icons.more),
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
