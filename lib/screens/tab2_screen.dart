0ce16b5edf3dd99e5dc295008e8afe53b485aa2a
Yuler Figueroa <yuleralex1@gmail.com>
2023-01-08 14:48:59 -0500

    change color selected category icon

--- lib/screens/tab2_screen.dart
+++ lib/screens/tab2_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_flutter_app/model/category_model.dart';
 import 'package:news_flutter_app/services/news_service.dart';
+import 'package:news_flutter_app/theme/theme_dark.dart';
 import 'package:provider/provider.dart';
 
 class Tab2Screen extends StatelessWidget {
@@ -59,22 +60,25 @@ class _CategoryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final newsService = Provider.of<NewsService>(context);
+
     return GestureDetector(
       onTap: () {
-        final  newsService = Provider.of<NewsService>(context, listen: false);
-        print(category.name);
+        newsService.selectedCategory = category.name;
       },
       child: Container(
         width: 40,
         height: 40,
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        decoration: BoxDecoration(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: const BoxDecoration(
           shape: BoxShape.circle,
           color: Colors.white,
         ),
         child: Icon(
           category.icon,
-          color: Colors.black54,
+          color: (category.name == newsService.selectedCategory)
+              ? myTheme.colorScheme.secondary
+              :  Colors.black54,
         ),
       ),
     );
